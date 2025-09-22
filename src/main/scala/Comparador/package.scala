6b60a3fc5d4f08a9d4c3419c39f26eab08d2ddc6
LisNat <rivera.liseth@correounivalle.edu.co>
2025-09-21 19:40:14 -0500

    Algoritmo de ordenamiento QuicSort

--- src/main/scala/Comparador/package.scala
+++ src/main/scala/Comparador/package.scala
@@ -34,12 +34,45 @@ package object Comparador {
     insertionSortInstance
     }
 
-  def comparar[T](a1:AlgoritmoOrd[T], a2:AlgoritmoOrd[T], l:List[T]):(Int, Int) = {
+  def menoresQue_noMenoresQue[T](l: List[T], v: T, comp: Comparador[T]): (List[T], List[T], Int) = {
+    @tailrec
+    def partitionIter(rest: List[T], menores: List[T], noMenores: List[T], ops: Int): (List[T], List[T], Int) = {
+      if (rest.isEmpty) (menores, noMenores, ops)
+      else {
+        val head = rest.head
+        val tail = rest.tail
+        if (comp(head, v))
+          partitionIter(tail, head :: menores, noMenores, ops + 1)
+          //partitionIter(tail, menores ++ List(head), noMenores, ops + 1)
+        else
+          partitionIter(tail, menores, head :: noMenores, ops + 1)
+          //partitionIter(tail, menores, noMenores ++ List(head), ops + 1)
+      }
+    }
+    partitionIter(l, List(), List(), 0)
+  }
+
+
+  def quickSort[T](comp: Comparador[T]): AlgoritmoOrd[T] = {
+    def quickSortInstance(l: List[T]): (List[T], Int) = {
+      if (l.isEmpty || l.tail.isEmpty) (l, 0)
+      else {
+        val pivot = l.head
+        val rest = l.tail
+        val (menores, noMenores, opsPartition) = menoresQue_noMenoresQue(rest, pivot, comp)
+        val (sortedMenores, opsMenores) = quickSortInstance(menores)
+        val (sortedNoMenores, opsNoMenores) = quickSortInstance(noMenores)
+        (sortedMenores ++ List(pivot) ++ sortedNoMenores, opsPartition + opsMenores + opsNoMenores)
+      }
+    }
+    quickSortInstance
+  }
+
+  def comparar[T](a1: AlgoritmoOrd[T], a2: AlgoritmoOrd[T], l: List[T]): (Int, Int) = {
     val (l1, c1) = a1(l)
     val (l2, c2) = a2(l)
 
-    if(l1==l2) (c1,c2) else (-1,-1)
+    if (l1 == l2) (c1, c2) else (-1, -1)
   }
-
 
 }
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -11,10 +11,30 @@ def randomList(long:Int): List[Int] ={
 }
 
 def menorQue(a:Int, b:Int): Boolean = a < b
+def mayorQue( a:Int, b:Int ): Boolean = a > b
+
 
 insert(7, List(2,3,4,8), menorQue)
 
-val iSort = insertionSort[Int](menorQue)
+val iSort_Asc = insertionSort[Int](menorQue)
+val iSort_Desc = insertionSort[Int](mayorQue)
 
-iSort(List(4,5,6,1,2,3))
-iSort(List(3,2,1,6,5,4))
+iSort_Asc(List(4,5,6,1,2,3))
+iSort_Asc(List(3,2,1,6,5,4))
+
+
+val qSort_Asc = quickSort[Int](menorQue)
+val qSort_Desc = quickSort[Int](mayorQue)
+
+qSort_Asc(List( 4, 5, 6, 1, 2, 3))
+qSort_Desc(List( 4, 5, 6, 1, 2, 3))
+
+val lAsc100 = (1 to 100).toList
+val lDsc100 = (1 to 100).toList.reverse
+
+comparar (iSort_Asc, qSort_Asc, lAsc100)
+comparar (iSort_Asc, qSort_Asc, lDsc100)
+
+val lista = List(4, 2, 5, 1)
+val pivote = 3
+val resultado = menoresQue_noMenoresQue(lista, pivote, menorQue)
